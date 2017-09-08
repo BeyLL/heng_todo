4dc10801e587b2cd55dc86c326e175d1879145a1
zhangweiheng <zhangheng@upvi.com>
2017-09-08 23:55:42 +0800

    跑通环境

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/**
+ * Created by "zhangHeng" on 2017/9/8 0008.
+ */
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react';
+import {render} from 'react-dom';
+import App from "./components/App";  //这里用到了解构
+
+render(
+    <div>
+        <App/>
+    </div>,document.getElementById('root')
+)
