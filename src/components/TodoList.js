507f304c26fe6e2b3f5f2aee9bb4eb779738bd97
zhangweiheng <zhangheng@upvi.com>
2017-09-09 15:42:42 +0800

    删除

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,7 @@
  */
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-
+import {DELETE_TODO} from '../store/action-types'
 class TodoList extends Component {
     constructor(props) {
         super(props)
@@ -13,17 +13,20 @@ class TodoList extends Component {
         return (
             <ul className="list-group">
                 {this.props.todos.map((item, index) => {
-                    return <li style={{listStyle:'none'}}>
+                    return (<li style={{listStyle:'none'}} key={index}>
                         <div className="list-group-item">
                             <input type="checkbox"/>
                             <span style={{marginLeft:'7px'}}>{item.title}</span>
                             <span className="pull-right">
-                                 <button className="btn btn-danger btn-xs">
+                                 <button className="btn btn-danger btn-xs" onClick={()=>{
+                                     this.props.deleteTodo(item.id);
+                                 }
+                                 }>
                                      x
                                  </button>
                             </span>
                         </div>
-                    </li>
+                    </li>)
                 })}
 
             </ul>
@@ -33,5 +36,8 @@ class TodoList extends Component {
 export default connect(
     state => ({
         todos: state.todos
+    }),
+    dispatch=>({
+        deleteTodo:(id)=>dispatch({type:DELETE_TODO,id})
     })
 )(TodoList)
--- src/store/reducers/todos.js
+++ src/store/reducers/todos.js
@@ -1,12 +1,16 @@
 /**
  * Created by "zhangHeng" on 2017/9/9 0009.
  */
-import {ADD_TODO} from '../action-types'
+import {ADD_TODO,DELETE_TODO} from '../action-types'
 //todo的reducer
 export default function (state = [], action) {
     switch (action.type) {
         case ADD_TODO:
             return [...state, {id: Date.now(), title: action.title, completed: false}];
+        case DELETE_TODO:
+            return state.filter(item=>{
+               return  item.id!=action.id
+            });
         default:
             return state;
     }
